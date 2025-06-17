354d4b0b6b3c31e67ab718d36bebffa69edf0415
Garita Arias <yahir.garitaarias@abbott.com>
2025-06-17 10:32:20 -0600

    Agregado campo Title a la respuesta de la API

--- txs.py
+++ txs.py
@@ -65,7 +65,8 @@ def optimizar_rutas_empleados(empleados):
                         "Lat": emp["DirecciónLAT"],
                         "Lon": emp["DirecciónLONG"],
                         "Fecha": emp["Fecha"],
-                        "Hora": emp["Hora entrada"]
+                        "Hora": emp["Hora entrada"],
+			"Title": emp["Title"]
                     })
             else:
                 for emp in grupo:
@@ -77,7 +78,8 @@ def optimizar_rutas_empleados(empleados):
                         "Lat": emp["DirecciónLAT"],
                         "Lon": emp["DirecciónLONG"],
                         "Fecha": emp["Fecha"],
-                        "Hora": emp["Hora entrada"]
+                        "Hora": emp["Hora entrada"],
+			"Title": emp["Title"]
                     })
         else:
             emp = grupo[0]
@@ -89,7 +91,8 @@ def optimizar_rutas_empleados(empleados):
                 "Lat": emp["DirecciónLAT"],
                 "Lon": emp["DirecciónLONG"],
                 "Fecha": emp["Fecha"],
-                "Hora": emp["Hora entrada"]
+                "Hora": emp["Hora entrada"],
+		"Title": emp["Title"]
             })
         grupo_id += 1
 
