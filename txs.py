5b7f533cde7b52e03baba89439945a9ef71381a6
Garita Arias <yahir.garitaarias@abbott.com>
2025-06-17 10:05:55 -0600

    Agregado campo Title a la respuesta de la API

--- txs.py
+++ txs.py
@@ -73,3 +73,7 @@ def optimizar_rutas():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/')
+def home():
+    return "¡API funcionando
